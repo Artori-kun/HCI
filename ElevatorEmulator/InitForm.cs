6793f14ec287116a9070b1ab30474d45a2b058b5
DuyMinhvn <cr7.duyminh1st@gmail.com>
2019-11-06 11:04:04 +0700

    up

--- ElevatorEmulator/InitForm.cs
+++ ElevatorEmulator/InitForm.cs
@@ -13,15 +13,37 @@ namespace ElevatorEmulator
     public partial class InitForm : Form
     {
         ConfigForm config;
+        Form1 frm;
         public InitForm()
         {
             InitializeComponent();
             config = new ConfigForm();
+            frm = new Form1();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //config.Show();
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
             config.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frm.Show();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            frm.Show();
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e)
+        {
+            config.Close();
         }
     }
 }
